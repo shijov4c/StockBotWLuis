682ba5d2ad2599eb1871d04a6f9decd29b127ad3
Shijo Varghese <Shijo.Varghese@velrada.com>
2018-04-30 12:01:06 +0800

    fisrt commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=stockbotwluis-Web-Deploy.pubxml -p:Password=3jM8lEFeiGwS7co60MQdbBqmrS3a1ePQpBFZAs5BgBtjy8WzmjKxBlsQW9q3
+
